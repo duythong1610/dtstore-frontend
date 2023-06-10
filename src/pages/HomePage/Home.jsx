16e4ee77938473618b20f1feac870157e7d7c65b
Âu Duy Thông <thongad.tdemr@gmail.com>
2023-06-11 02:39:25 +0700

    ...

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -150,7 +150,7 @@ function Home() {
     <>
       <Helmet>
         <title>Trang chủ | October16th</title>
-        <script src="/src/scriptMessenger.js"></script>
+        <script src="/src/scriptMessenger.js" type="text/javascript"></script>
       </Helmet>
       {/* <Loading isLoading={isLoading}> */}
 
