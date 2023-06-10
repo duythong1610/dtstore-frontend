1973cc195f37f05846b5870a64b3ce1e6a71b13c
Âu Duy Thông <thongad.tdemr@gmail.com>
2023-06-11 03:50:47 +0700

    ...

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -54,45 +54,6 @@ function Home() {
   const [loading, setLoading] = useState(false);
   const [loadingLimit, setLoadingLimit] = useState(false);
 
-  useEffect(() => {
-    if (window.location.pathname === "/") {
-      const script = document.createElement("script");
-      script.innerHTML = `
-      var chatbox = document.getElementById("fb-customer-chat");
-      chatbox.setAttribute("page_id", "101501822338558");
-      chatbox.setAttribute("attribution", "biz_inbox");
-
-      <!-- Your SDK code -->
-
-      window.fbAsyncInit = function () {
-        FB.init({
-          xfbml: true,
-          version: "v17.0",
-        });
-      };
-
-      (function (d, s, id) {
-        var js,
-          fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      })(document, "script", "facebook-jssdk");
-    `;
-      document.body.appendChild(script);
-
-      return () => {
-        document.body.removeChild(script);
-        script.innerHTML = `
-      var chatbox = document.getElementById("fb-customer-chat");
-      chatbox.removeAttribute("page_id");
-      chatbox.removeAttribute("attribution");`;
-      };
-    }
-  }, []);
-
   const handleGetUserDetails = async () => {
     const res = await UserService.getDetailsUser(user?.id, user?.access_token);
     setUserInfo(res.data);
