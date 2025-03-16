fced64d86dd227b50e8cf94f49ddf7d64959d818
Âu Duy Thông <auduythong1610@gmail.com>
2025-03-16 22:36:11 +0700

    Update: config proxy

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 
     proxy: {
       "/api": {
-        target: "https://api.october16th.store",
+        target: "https://api-october16th-store.up.railway.app",
       },
     },
   },
